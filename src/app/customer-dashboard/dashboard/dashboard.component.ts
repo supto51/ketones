114d3fbe686fb6573b724669e47e8cd20f2d961e
soirbg <stoyan.dobrev@eldersoss.com>
2022-02-15 11:12:16 +0200

    taking user for dashboard

--- src/app/customer-dashboard/dashboard/dashboard.component.ts
+++ src/app/customer-dashboard/dashboard/dashboard.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserEmitterService } from 'src/app/shared/services/user-emitter.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  public user: any;
+  constructor(private userEmitterService: UserEmitterService) {}
 
   ngOnInit(): void {
+    this.userEmitterService.getProfileObs().subscribe(x => {
+      this.user = x;
+    });
   }
-
 }
